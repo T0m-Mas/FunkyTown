aec5e9dbd8e136987f53f6ec961daab417a94235
Tom <tomas_masotta@live.com>
2021-06-26 22:22:16 -0300

    Revert "Subo Avances"
    This reverts commit 8490079b8e529711e8815f8a05b718e5ab6109b1.

--- controllers/pedidoOk.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require '../fw/fw.php';
-require '../views/PedidoOk.php';
-
-$v = new PedidoOK();
-$v->render();
--- static/js/carrito.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-document.getElementById("return_home").onclick = function(){
-	window.location.href = "home";
-}
