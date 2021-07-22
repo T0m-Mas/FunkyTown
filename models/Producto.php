c3cc741e43c7bde0517ac709c3562e309419efae
Tom <tomas_masotta@live.com>
2021-07-22 15:36:00 -0300

    Revicion Validaciones 22_07

--- models/Producto.php
+++ models/Producto.php
@@ -111,10 +111,12 @@ class Producto extends Model{
 
 		if(!ctype_digit($categoria)) throw new ValidacionException("errAlta5");
 		if($categoria<=0) throw new ValidacionException("errAlta6");
+		$c = new Categoria();
+		if(!$c->existe($categoria)) throw new ValidacionException("errAlta7");
 
 		$precio = str_replace(',','.',$precio);
-		if(!is_numeric($precio)) throw new ValidacionException("errAlta7");
-		if($precio<0) throw new ValidacionException("errAlta8");
+		if(!is_numeric($precio)) throw new ValidacionException("errAlta8");
+		if($precio<0) throw new ValidacionException("errAlta9");
 
 		$this->db->query(  "INSERT INTO producto (titulo,descripcion,id_categoria,precio_venta,img) 
 							VALUES ('$titulo','$descripcion', $categoria,'$precio','$img')");
@@ -123,13 +125,13 @@ class Producto extends Model{
 	}
 
 	public function setTitulo($id,$titulo){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetTitulo1");
+		if($id<=0) throw new ValidacionException("errsetTitulo2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetTitulo3");
 
-		if(strlen($titulo)==0) return;
-		if(strlen($titulo)>40) return;
+		if(strlen($titulo)==0) throw new ValidacionException("errsetTitulo4");
+		if(strlen($titulo)>40) throw new ValidacionException("errsetTitulo5");
 		$titulo = $this->db->escape($titulo);
 		$titulo = htmlentities($titulo);
 
@@ -140,13 +142,13 @@ class Producto extends Model{
 	}
 
 	public function setDescripcion($id,$descripcion){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetDescripcion1");
+		if($id<=0) throw new ValidacionException("errsetDescripcion2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetDescripcion3");
 
-		if(strlen($descripcion)==0) return;
-		if(strlen($descripcion)>256) return;
+		if(strlen($descripcion)==0) throw new ValidacionException("errsetDescripcion4");
+		if(strlen($descripcion)>256) throw new ValidacionException("errsetDescripcion5");
 		$descripcion = $this->db->escape($descripcion);
 		$descripcion = htmlentities($descripcion);
 
@@ -155,49 +157,49 @@ class Producto extends Model{
 	}
 
 	public function setCategoria($id,$categoria){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetCategoria1");
+		if($id<=0) throw new ValidacionException("errsetCategoria2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetCategoria3");
 
-		if(!ctype_digit($categoria)) return;
-		if($categoria<=0) return;
+		if(!ctype_digit($categoria)) throw new ValidacionException("errsetCategoria4");
+		if($categoria<=0) throw new ValidacionException("errsetCategoria5");
 		$c = new Categoria();
-		if(!$c->existe($categoria)) return;
+		if(!$c->existe($categoria)) throw new ValidacionException("errsetCategoria6");
 
 		$this->db->query("UPDATE producto SET id_categoria = $categoria WHERE id = $id");
 
 	}
 
 	public function setPrecio($id,$precio){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetPrecio1");
+		if($id<=0) throw new ValidacionException("errsetPrecio2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetPrecio3");
 
 		$precio = str_replace(',','.',$precio);
-		if(!is_numeric($precio)) return;
-		if($precio<0) return;
+		if(!is_numeric($precio)) throw new ValidacionException("errsetPrecio4");
+		if($precio<0) throw new ValidacionException("errsetPrecio5");
 
 		$this->db->query("UPDATE producto SET precio_venta = '$precio' WHERE id = $id");
 
 	}
 
 	public function setImagen($id,$img){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetImagen1");
+		if($id<=0) throw new ValidacionException("errsetImagen2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetImagen3");
 
 		$this->db->query("UPDATE producto SET img = '$img' WHERE id = $id");
 
 	}
 
 	public function eliminar($id){
-		if(!ctype_digit($id)) return;
-		if($id<=0) return;
+		if(!ctype_digit($id)) throw new ValidacionException("errsetEliminar1");
+		if($id<=0) throw new ValidacionException("errsetEliminar2");
 		$this->db->query("SELECT id FROM producto WHERE id = $id");
-		if($this->db->numRows()==0) return;
+		if($this->db->numRows()==0) throw new ValidacionException("errsetEliminar3");
 
 		$this->db->query("DELETE FROM producto WHERE id = $id");
 	}
